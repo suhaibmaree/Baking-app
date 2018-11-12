1c87564ef147d5f389cc28f0c47d789c56e2f085
suhaib <suhaib.sym.m@gmail.com>
2018-11-12 21:00:55 +0200

    Fix Widget onClick issue

--- app/src/main/java/com/example/suhaib/bakingapp/BakingWidget.java
+++ app/src/main/java/com/example/suhaib/bakingapp/BakingWidget.java
@@ -1,26 +1,38 @@
 package com.example.suhaib.bakingapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class BakingWidget extends AppWidgetProvider {
+    public static String ingredian = "ingredians";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId, String ingredians) {
+        ingredian= ingredians;
 
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
         views.setTextViewText(R.id.appwidget_text, ingredians);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        Intent intent = new Intent(context,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId,views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds){
+            updateAppWidget(context,appWidgetManager,appWidgetId,ingredian);
+        }
 
     }
 
